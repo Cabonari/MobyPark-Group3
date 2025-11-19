8131af49e70a38f6efda9fc67332eb8d85b5564d
JonathanHSR <1080312@hr.nl>
2025-11-19 09:04:54 +0100

    Organised test files

--- Code/Parking-api/api/tests/test_storage_utils.py
+++ Code/Parking-api/api/tests/test_storage_utils.py
@@ -5,7 +5,7 @@ import sys
 from unittest.mock import patch, mock_open, call
 
 CURRENT_DIR = os.path.dirname(__file__)
-PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, '..', '..'))
+PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, '..'))
 
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
