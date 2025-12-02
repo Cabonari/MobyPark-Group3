37dc39dacfc7d3545998855ecb7396dd379433f1
Rushillachmon <219551871+Rushlachmon@users.noreply.github.com>
2025-12-02 13:04:36 +0100

    hi

--- Code/Parking-api/api/tests/integration/parking_lots/test_profile.py
+++ Code/Parking-api/api/tests/integration/parking_lots/test_profile.py
@@ -3,7 +3,7 @@ import requests
 import hashlib; 
 
 @pytest.fixture
-def api():
+def api(): 
     """Return base URL and headers for testing."""
     url = "http://localhost:8000"
     headers = {"Authorization": "abc123", "Content-Type": "application/json"}
