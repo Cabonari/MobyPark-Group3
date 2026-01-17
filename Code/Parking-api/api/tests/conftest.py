f64d863bcb7470ec48e6ce587156ca33414243c8
IvarsCode <ihendriks135@gmail.com>
2026-01-17 14:46:41 +0100

    correct path add test session

--- Code/Parking-api/api/tests/conftest.py
+++ Code/Parking-api/api/tests/conftest.py
@@ -1,13 +1,11 @@
 import pytest
-from api.session_manager import add_session
+from ..session_manager import add_session
 
 
-# this is to create the session that tests use to run
 @pytest.fixture(scope="session", autouse=True)
 def test_session():
     """
     Create a default test session so Authorization: abc123 works.
-    Runs once before all tests.
     """
     add_session(
         "abc123",
